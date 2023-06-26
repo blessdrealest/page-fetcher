39226498626087c738f4399b6eebb1a99b4f500a
Blessing <dijiblessing2@hotmail.co.uk>
2023-06-26 09:07:15 -0600

    developed fs statement and refactored code

--- fetcher.js
+++ fetcher.js
@@ -3,14 +3,23 @@ download resource at URL to local path on machine
 print out msg like "Downloaded and saved 1235 bytes to ./index.html"
 */
 
-const request = require ('request');
+const request = require('request');
 const fs = require("fs");
 
-const path = process.argv[3];
-const domain = process.argv [2];
+const localPath = process.argv[3];
+const domain = process.argv[2];
 
 request(domain, (error, response, body) => {
   if (error) {
     console.log('error: ', error);
   }
-})
+
+  fs.writeFile(`${localPath}`, body, function (error) {
+    if (error) {
+      console.log('error', error);
+    } else {
+      console.log(`Downloaded and saved ${response.headers["content-length"]} bytes to ${localPath}`);
+    }
+  });
+});
+
--- requestPage.js
+++ requestPage.js
@@ -1,5 +1,5 @@
 const request = require('request');
-request('https://www.google.com/fdsafsafsa.html', (error, response, body) => {
+request('http://www.example.edu/', (error, response, body) => {
   console.log('error:', error); // Print the error if one occurred
   console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
   console.log('body:', body); // Print the HTML for the Google homepage.
